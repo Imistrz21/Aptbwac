fccebb67669dfe185527df16a83c19bc9d88f107
Imistrz <141686480+Imistrz21@users.noreply.github.com>
2024-04-12 22:09:07 +0200

    Update program.bat

--- program.bat
+++ program.bat
@@ -21,24 +21,30 @@ if exist "%file_path%" (
     echo 3. Other programs bypass
 
     set /p bypass_choice="Enter the bypass type (1, 2, or 3): "
-    rem 
     set "bypass_choice=%bypass_choice: =%"
     echo Bypass choice is: %bypass_choice%
     
     if "%bypass_choice%"=="1" (
         set bypass_type=Steam
+        echo Running "%file_path%" with Steam bypass...
+        cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "%file_path%""
+        exit
     ) else if "%bypass_choice%"=="2" (
         set bypass_type=Epic Games
+        echo Running "%file_path%" with Epic Games bypass...
+        cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "%file_path%""
+        exit
     ) else if "%bypass_choice%"=="3" (
         set bypass_type=Other programs
+        echo Running "%file_path%" with Other programs bypass...
+        cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "%file_path%""
+        exit
     ) else (
         echo Invalid bypass type. Please choose 1, 2, or 3.
         goto choose_bypass
     )
 
-    echo Running "%file_path%" with %bypass_type% bypass...
-    cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "%file_path%""
-    exit
+
 ) else (
     color c
     echo File not found.
