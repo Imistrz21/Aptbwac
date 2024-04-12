495b7c6a4f0778c214086b823e75a70bbfa39a88
Imistrz <141686480+Imistrz21@users.noreply.github.com>
2024-04-13 00:06:08 +0200

    Update program.bat
    remake some of the core functions, better .exe support

--- program.bat
+++ program.bat
@@ -1,59 +1,92 @@
 @echo off
+setlocal enabledelayedexpansion
+
+rem Check if the script is running with admin privileges
+net session >nul 2>&1
+if %errorlevel% equ 0 (
+    color 9
+    echo Loading [.]
+    timeout /t 1 >nul
+    color b
+    echo Loading [..]
+    timeout /t 1 >nul
+    color c
+    timeout /t 1 >nul
+    color c
+    timeout /t 1 >nul
+    cls
+    echo Credit to Imistrz21 on GitHub
+    echo ==================================
+    echo Why u use dis script with admin????
+    echo ==================================
+    echo Credit to Imistrz21 on GitHub
+    echo.
+    timeout /t 3 >nul
+    exit
+)
 color 9
-echo Loading [.]
-timeout /t 1 >nul
-color b
-echo Loading [..]
-timeout /t 1 >nul
-color a
-echo Loading [...]
-timeout /t 1 >nul
-color
+    echo Loading [.]
+	set "file_path="
+    timeout /t 1 >nul
+    color b
+    echo Loading [..]
+	set "file_path="
+    timeout /t 1 >nul
+    color a
+    echo Loading [...]
+	set "file_path="
+    timeout /t 1 >nul
+    color
+    cls
+
+:main
 cls
-color a
+color a 
 echo Credit to Imistrz21 on GitHub
 echo ==================================
-echo Welcome!! 
+echo Welcome!!
 echo Everything initialized properly!!!
 echo ==================================
 echo Credit to Imistrz21 on GitHub
 echo.
 pause
 cls
-:main
-setlocal
-
-set file_path=%1
 
 :check_file_path
-if "%file_path%"=="" (
-    echo Credit to Imistrz21 on GitHub
-    echo ==================================
-    echo The file path should look like this: "C:\example\example.exe"
-    set /p file_path="Enter the file path: "
-    echo ==================================
-    echo Credit to Imistrz21 on GitHub
-)
+cls
+color a 
+echo Credit to Imistrz21 on GitHub
+echo ==================================
+echo The file path should look like this: "C:\example\example.exe"
+set /p file_path="Enter the file path: "
 
-if not exist "%file_path%" (
-    cls	
-    color c
-    echo Credit to Imistrz21 on GitHub
-    echo ==================================
-    echo File not found. Please enter a valid file path.
-    echo ==================================
-    echo Credit to Imistrz21 on GitHub
-    echo.
-    setlocal
-    set file_path=%1
-    pause
-    cls
-    color a
-    goto check_file_path
-)
+if "%file_path%"=="" goto invalid_path
+if not exist "%file_path%" goto invalid_path
+
+rem Check if the file_path points to a folder
+if exist "%file_path%\*" goto invalid_path
+
+set "valid_extensions=.exe .bat .cmd .msi"
+set "file_extension=%file_path:~-4%"
+if "%valid_extensions%" equ "!valid_extensions:%file_extension%=!" goto invalid_path
+
+goto show_menu
+
+:invalid_path
+cls
+color c
+echo Credit to Imistrz21 on GitHub
+echo ==================================
+echo Invalid file path. Please enter a valid file path.
+echo ==================================
+echo Credit to Imistrz21 on GitHub
+echo.
+pause
+goto check_file_path
 
 :show_menu
 cls
+color a
 echo Credit to Imistrz21 on GitHub
 echo ==================================
 echo Choose the bypass type:
@@ -91,6 +124,7 @@ if "%bypass_choice%"=="1" (
     pause
     goto show_menu
 )
+
 echo Credit to Imistrz21 on GitHub
 echo ==================================
 echo Running "%file_path%" with %bypass_type% bypass...
