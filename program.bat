d35a0a519693a50f3bd68ff4418972c6753c9d7c
Imistrz <141686480+Imistrz21@users.noreply.github.com>
2024-04-12 22:01:54 +0200

    Update program.bat
    In this version the selection works only the second time.

--- program.bat
+++ program.bat
@@ -5,16 +5,44 @@ setlocal
 
 set file_path=%1
 
+:check_file_path
 if "%file_path%"=="" (
     set /p file_path="Enter the file path: "
 )
 
 if exist "%file_path%" (
-    echo File found. Running "%file_path%"
+    echo File found.
+    
+    :choose_bypass
+    set bypass_choice=
+    echo Choose the bypass type:
+    echo 1. Steam bypass
+    echo 2. Epic Games bypass
+    echo 3. Other programs bypass
+
+    set /p bypass_choice="Enter the bypass type (1, 2, or 3): "
+    rem Remove leading and trailing whitespace characters
+    set "bypass_choice=%bypass_choice: =%"
+    echo Bypass choice is: %bypass_choice%
+    
+    if "%bypass_choice%"=="1" (
+        set bypass_type=Steam
+    ) else if "%bypass_choice%"=="2" (
+        set bypass_type=Epic Games
+    ) else if "%bypass_choice%"=="3" (
+        set bypass_type=Other programs
+    ) else (
+        echo Invalid bypass type. Please choose 1, 2, or 3.
+        goto choose_bypass
+    )
+
+    echo Running "%file_path%" with %bypass_type% bypass...
     cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "%file_path%""
+    exit
 ) else (
     color c
     echo File not found.
+    goto check_file_path
 )
 
 pause
